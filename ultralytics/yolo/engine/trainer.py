476c7221f1eea254eec7a723d49164f3a5e5d04c
Lingcao Huang <huanglingcao@gmail.com>
2023-01-27 14:26:05 -0700

    remvoe assert, dont throw an error

--- ultralytics/yolo/engine/trainer.py
+++ ultralytics/yolo/engine/trainer.py
@@ -537,9 +537,10 @@ class BaseTrainer:
             self.ema.ema.load_state_dict(ckpt['ema'].float().state_dict())  # EMA
             self.ema.updates = ckpt['updates']
         if self.resume:
-            assert start_epoch > 0, \
+            if start_epoch > 0:
                 f'{self.args.model} training to {self.epochs} epochs is finished, nothing to resume.\n' \
                 f"Start a new training without --resume, i.e. 'yolo task=... mode=train model={self.args.model}'"
+                return
             LOGGER.info(
                 f'Resuming training from {self.args.model} from epoch {start_epoch} to {self.epochs} total epochs')
         if self.epochs < start_epoch:
